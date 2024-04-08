f743f00362cee87f9f5bb0da7eac6d49027143fa
Kimdongjun <ejuniya@naver.com>
2024-04-08 22:37:41 +0900

    New!

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from nltk.tokenize import sent_tokenize
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-
+#github
 raw_text = "A barber is a person. a barber is good person. a barber is huge person. he Knew A Secret! The Secret He Kept is huge secret. Huge secret. His barber kept his word. a barber kept his word. His barber kept his secret. But keeping and keeping such a huge secret to himself was driving the barber crazy. the barber went up a huge mountain."
 
 sentences = sent_tokenize(raw_text)
